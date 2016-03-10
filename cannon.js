5d6b06fcfe52759727f2616433435c5181fc1083
Alex 'gags' Hogue <ahogue@atlassian.com>
2016-03-10 13:36:04 +1100

    Change cannonball and make targets float

--- cannon.js
+++ cannon.js
@@ -1,4 +1,11 @@
+
+SHOOT_TIMER_SECONDS = 1;
+
 window.addEventListener('load', function(e) {
+
+    state = {};
+    state.level = 0;
+
     var Q = window.Q = Quintus()
         .include('Input,Sprites,Scenes,SVG,Physics')
         .svgOnly()
@@ -11,13 +18,13 @@ window.addEventListener('load', function(e) {
         init: function(props) {
             this._super({
                 shape: 'circle',
-                color: 'red',
-                r: 8,
+                color: 'black',
+                r: 15,
                 restitution: 0.5,
                 density: 4,
                 x: props.dx * 50 + 10,
                 y: props.dy * 50 + 210,
-                seconds: 5
+                seconds: 30
             });
             this.add('physics');
             this.on('step', this, 'countdown');
@@ -52,11 +59,23 @@ window.addEventListener('load', function(e) {
                     [0, 5]
                 ],
                 x: 10,
-                y: 210
+                y: 210,
             });
+            this.fireVelocity = 1000;
+            this.canShootTimer = SHOOT_TIMER_SECONDS;
+            this.on('step', this, 'increaseShootTimer');
+        },
+
+        increaseShootTimer: function(dt) {
+            this.canShootTimer += dt;
         },
 
         fire: function() {
+            // Can't fire away yet, got ships to board.
+            if (this.canShootTimer < SHOOT_TIMER_SECONDS) {
+                return
+            }
+
             var dx = Math.cos(this.p.angle / 180 * Math.PI),
                 dy = Math.sin(this.p.angle / 180 * Math.PI),
                 ball = new Q.CannonBall({
@@ -65,138 +84,36 @@ window.addEventListener('load', function(e) {
                     angle: this.p.angle
                 });
             Q.stage().insert(ball);
-            ball.physics.velocity(dx * 400, dy * 400);
+            ball.physics.velocity(dx * this.fireVelocity, dy * this.fireVelocity);
+            this.canShootTimer = 0;
         }
     });
 
-    var targetCount = 0;
     Q.Sprite.extend('Target', {
         init: function(props) {
             this._super(Q._extend(props, {
                 shape: 'circle',
                 color: 'pink',
-                r: 8
+                r: 8,
+                type: 'static'
             }));
-            targetCount++;
             this.add('physics');
             this.on('contact', this, 'checkHit');
         },
 
         checkHit: function(sprite) {
             if (sprite instanceof Q.CannonBall) {
-                targetCount--;
                 this.destroy();
-                if (targetCount == 0) {
-                    Q.stageScene('level');
-                }
+                state.level++;
+                Q.stageScene('level' + state.level);
             }
         }
     });
 
 
+    loadLevel0();
 
-
-    Q.scene('level', new Q.Scene(function(stage) {
-        targetCount = 0;
-        stage.add("world");
-        stage.insert(new Q.Sprite({
-            x: 250,
-            y: 250,
-            w: 700,
-            h: 50,
-            type: "static"
-        }))
-
-        stage.insert(new Q.Sprite({
-            w: 10,
-            h: 50,
-            x: 500,
-            y: 200
-        }));
-        stage.insert(new Q.Sprite({
-            w: 10,
-            h: 50,
-            x: 550,
-            y: 200
-        }));
-        stage.insert(new Q.Sprite({
-            w: 70,
-            h: 10,
-            x: 525,
-            y: 170
-        }));
-        stage.insert(new Q.Sprite({
-            w: 10,
-            h: 50,
-            x: 500,
-            y: 130
-        }));
-        stage.insert(new Q.Sprite({
-            w: 10,
-            h: 50,
-            x: 550,
-            y: 130
-        }));
-        stage.insert(new Q.Sprite({
-            w: 70,
-            h: 10,
-            x: 525,
-            y: 110
-        }));
-
-        stage.insert(new Q.Sprite({
-            points: [
-                [0, 0],
-                [50, -50],
-                [150, -50],
-                [200, 0]
-            ],
-            x: 200,
-            y: 225,
-            type: 'static',
-            shape: 'polygon'
-        }));
-
-        stage.insert(new Q.Sprite({
-            w: 50,
-            h: 50,
-            x: 300,
-            y: 150
-        }));
-        stage.insert(new Q.Sprite({
-            w: 25,
-            h: 25,
-            x: 300,
-            y: 115
-        }));
-
-        stage.each(function() {
-            this.add("physics");
-        });
-
-        stage.insert(new Q.Target({
-            x: 525,
-            y: 90
-        }));
-        stage.insert(new Q.Target({
-            x: 300,
-            y: 90
-        }));
-        stage.insert(new Q.Sprite({
-            w: 30,
-            h: 30,
-            x: 10,
-            y: 210,
-            color: 'blue'
-        }));
-
-        stage.cannon = stage.insert(new Q.Cannon());
-        stage.viewport(600, 400);
-        stage.centerOn(300, 100);
-
-    }));
-
-    Q.stageScene("level");
+    Q.stageScene("level0");
     var cannonMove = function(e) {
         var stage = Q.stage(0),
             cannon = stage.cannon,
@@ -209,6 +126,7 @@ window.addEventListener('load', function(e) {
         cannon.p.angle = angle * 180 / Math.PI;
         e.preventDefault();
     };
+
     Q._each(["touchstart", "mousemove", "touchmove"], function(evt) {
         Q.wrapper.addEventListener(evt, cannonMove);
     }, this);
@@ -220,5 +138,8 @@ window.addEventListener('load', function(e) {
     Q._each(["touchend", "mouseup"], function(evt) {
         Q.wrapper.addEventListener(evt, canonFire);
     });
+
+
+
 
 });
